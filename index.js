35ea4f5c7b4e9561054831db6c4d5f92eff27d45
Sakshi Yadav <ydsakshi023@gmail.com>
2021-02-22 20:23:02 +0530

    adding only client side changes

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
-
+const APIAI_TOKEN = '4a110724556fe136a84012800161a4f3275713a7';
+const APIAI_SESSION_ID = '12';
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(5000);
+const server = app.listen(5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+// const apiai = require('apiai')(APIAI_TOKEN);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
+
+io.on('connection', function (socket) {
+    socket.on('chat message', (text) => {
+        // let apiaiReq = apiai.textRequest(text, {
+        //     sessionId: APIAI_SESSION_ID
+        // });
+        // apiaiReq.on('response', (response) => {
+        //     let aiText = response.result.fulfillment.speech;
+        //     socket.emit('bot reply', aiText); // Send the result back to the browser!
+        // });
+        console.log("what you said:",text);
+        // if(text.includes('not')) socket.emit('bot reply', 'I don\'t understand you');
+        // else socket.emit('bot reply', 'I can hear you..');
+        socket.emit('bot reply', 'You said :' + text);
+        // apiaiReq.on('error', (error) => {
+        //     console.log(error);
+        // });
+
+        // apiaiReq.end();
+    })
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,45 @@
-const SpeechRecognition = window.SpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition ;
 const recognition = new SpeechRecognition();
+
 const socket = io();
+
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
 
 document.querySelector('button').addEventListener('click', () => {
     recognition.start();
 });
+recognition.addEventListener('speechstart', () => {
+    console.log('Speech has been detected.');
+});
 
-recognition.addEventListener('result',(e) => {
+recognition.addEventListener('result', (e) => {
     let last = e.results.length - 1;
     let text = e.results[last][0].transcript;
-    
+    console.log(text);
     console.log('Confidence: ' + e.results[0][0].confidence);
-
-    socket.emit('chat message',text);
+    outputYou.textContent = text;
+    socket.emit('chat message', text);
 });
+
+recognition.addEventListener('speechend', () => {
+    console.log("Well you speech ended..");
+    recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+    outputBot.textContent = 'Error: ' + e.error;
+});
+
+function synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    synth.speak(utterance);
+}
+
+socket.on('bot reply', function (replyText) {
+    synthVoice(replyText);
+    if (replyText == '') replyText = '(No answer...)';
+    outputBot.textContent = replyText;
+})
