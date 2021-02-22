1f74d684fdeca03739845e3b30cf992e6c58f39f
Sakshi Yadav <ydsakshi023@gmail.com>
2021-02-23 00:01:05 +0530

    fixing port details

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const APIAI_SESSION_ID = '12';
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
